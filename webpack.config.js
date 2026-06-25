930a916eef8552a7a22b3bead5427ca7a1718380
olgasadyreva <olga.sadyreva@gmail.com>
2026-06-25 10:26:53 +0500

    add timer

--- webpack.config.js
+++ webpack.config.js
@@ -50,14 +50,6 @@ module.exports = {
 				test: /\.(png|svg|jpg|jpeg|gif|woff|woff2|eot|ttf|otf)$/i,
 				type: 'asset/resource',
 			},
-			// {
-			// 	test: /\.(png|svg|jpg|jpeg|gif)$/i,
-			// 	type: 'asset/resource',
-			// },
-			// {
-			// 	test: /\.(woff|woff2|eot|ttf|otf)$/i,
-			// 	type: 'asset/resource',
-			// },
 		],
 	},
 }
