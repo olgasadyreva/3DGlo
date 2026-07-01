4ad584678840b2ab7735261b094815d20702d81b
olgasadyreva <olga.sadyreva@gmail.com>
2026-07-01 10:29:05 +0500

    fix: add one handler for menu

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,30 +1,36 @@
 const menu = () => {
 	const menuBtn = document.querySelector('.menu')
 	const menu = document.querySelector('menu')
+	const menuList = menu.querySelector('ul')	
 
-	const menuList = menu.querySelector('ul')
-	const closeBtn = menu.querySelector('.close-btn')
-	const menuItems = menu.querySelectorAll('ul>li>a')
-
-	const handleMenu = () => {
-		menu.classList.toggle('active-menu')
-	}
-
-	menuBtn.addEventListener('click', handleMenu)
-
-	menu.addEventListener('click', e => {
+	const handleMenu = (e) => {
 		const target = e.target
-
+		
+		if (target === menuBtn || menuBtn.contains(target)) {
+			menu.classList.toggle('active-menu')
+			e.preventDefault()
+			return
+		}
+		
 		if (target.classList.contains('close-btn')) {
 			menu.classList.remove('active-menu')
 			e.preventDefault()
 			return
 		}
-
+		
 		if (target.tagName === 'A' && menuList.contains(target)) {
 			menu.classList.remove('active-menu')
+			return
 		}
-	})
+
+		if (menu.classList.contains('active-menu')) {
+			if (!menu.contains(target) && target !== menuBtn && !menuBtn.contains(target)) {
+				menu.classList.remove('active-menu')
+			}
+		}
+	}
+
+	document.addEventListener('click', handleMenu)
 }
 
 export default menu
